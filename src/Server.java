8eaeb3a965546e4575e9c2d82b2be99453e1ce6e
samee1528 <dineshkosala1528@gmail.com>
2022-08-16 16:10:02 +0530

    Content Updated

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
     public static void main(String[] args) {
-        
+        final ServerSocket serverSocket;
+        final Socket clientSocket;
+        final BufferedReader in;
+        final PrintWriter out;
+        final Scanner sc=new Scanner(System.in);
+
+    try {
+        serverSocket = new ServerSocket(5000);
+        clientSocket =serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        Thread sender= new Thread(new Runnable() {
+            String msg; // variable writter by the user
+            @Override
+            public void run() {
+                while (true){
+                    msg = sc.nextLine(); // reads data from user's keybord
+                    out.println(msg); // clientSocket
+                    out.flush(); // sending
+
+                }
+
+            }
+        });
+        sender.start();
+
+    } catch (IOException e){
+        e.printStackTrace();
     }
+
+    }
+
+
 }
